9e6809ca9b937aea420abb3f20928b65ebaa6569
Muhammad Sidratul Muntaha <muhammad.mochi@gmail.com>
2013-06-06 16:08:16 +0700

    commit sql

--- src/main/webapp/database/toko.sql
+++ src/main/webapp/database/toko.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Inang: 127.0.0.1
--- Waktu pembuatan: 28 Mei 2013 pada 11.29
+-- Waktu pembuatan: 06 Jun 2013 pada 11.07
 -- Versi Server: 5.5.27
 -- Versi PHP: 5.4.7
 
@@ -45,6 +45,58 @@ INSERT INTO `produk` (`id`, `kodeProduk`, `nama`, `harga`, `terakhirUpdate`) VAL
 (3, 'p00-1311445914', 'namax-928725786', 10000.00, '2013-05-28 00:00:00'),
 (4, 'p00761812158', 'namax-671483955', 10000.00, '2013-05-28 00:00:00'),
 (5, 'p00745814884', 'namax-1153022134', 10000.00, '2013-05-28 00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `enable` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `enable`) VALUES
+(1, 'sidratul', '123456789', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user_roles`
+--
+
+CREATE TABLE IF NOT EXISTS `user_roles` (
+  `userRoleId` int(10) NOT NULL AUTO_INCREMENT,
+  `userId` int(10) NOT NULL,
+  `authority` varchar(45) NOT NULL,
+  PRIMARY KEY (`userRoleId`),
+  KEY `FK_user_oles` (`userId`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data untuk tabel `user_roles`
+--
+
+INSERT INTO `user_roles` (`userRoleId`, `userId`, `authority`) VALUES
+(1, 1, 'ROLE_USER');
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `user_roles`
+--
+ALTER TABLE `user_roles`
+  ADD CONSTRAINT `FK_user_roles` FOREIGN KEY (`userId`) REFERENCES `users` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
